e4b0d619b055b11880b57d63cf09a1a34ff23ae9
Justin Donaldson <donaldson@bigml.com>
2012-07-16 14:20:48 -0700

    updated docs and bundle command

--- bundle.sh
+++ bundle.sh
@@ -1,5 +1,9 @@
-cd src; 
-haxe -js x.js --no-output -cp src -xml ../haxedoc.xml `ls js/phantomjs/*`
-zip -r ../phantom.zip . ../haxelib.xml ../haxedoc.xml
+rm -r haxelib
+rm phantomjs.zip
+mkdir -p haxelib
+cp haxelib.xml haxelib
+cp -r src/js haxelib
+cd src
+haxe -js x --no-output -xml ../haxelib/haxedoc.xml `ls js/phantomjs/*.hx`
 cd -
-
+zip -r phantomjs.zip haxelib -x "*.DS_Store"
