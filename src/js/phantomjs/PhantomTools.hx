143f4de8d745900242b266ff8a59145887780f72
Justin Donaldson <donaldson@bigml.com>
2012-07-18 15:49:31 -0700

    simplified check for phantom

--- src/js/phantomjs/PhantomTools.hx
+++ src/js/phantomjs/PhantomTools.hx
@@ -6,15 +6,11 @@ import js.phantomjs.WebPage;
  **/
 
 class PhantomTools {
-    private static var __PHANTOMJS__:Bool; 
 
     /**
       injects the currently executing phantomjs script into a webkit page.
      **/
     public static function injectThis(page:WebPage){
-        page.evaluate(function(){
-            untyped window.__PHANTOMJS__ = true; 
-        });
         page.injectJs(System.args[0]);
     }
 
@@ -22,7 +18,7 @@ class PhantomTools {
       Simple routine which detects if it is being evaluated in webkit.
       Note: The page must first be injected with injectThis.
      **/
-    public static function inWebkit(){
-        return untyped window.__PHANTOMJS__ != null;
+    public static function inPhantom(){
+        return untyped window.hasOwnProperty('phantom');
     }
 }
