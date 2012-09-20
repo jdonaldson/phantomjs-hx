9db9c4571c6769ee312fdd06875c3c0d25ad035a
Justin Donaldson <donaldson@bigml.com>
2012-09-20 13:18:40 -0700

    Fix injectThis, add page error "unhandler"
    Also, make the inPhantom() method no longer require injectThis().

--- src/js/phantomjs/PhantomTools.hx
+++ src/js/phantomjs/PhantomTools.hx
@@ -1,25 +1,43 @@
 package js.phantomjs;
 import js.phantomjs.WebPage;
-/**
-  A class with some utility functions to bootstrap haxe std lib into a given
-  page.
- **/
+import js.Dom.Window;
 
+/**
+  A class providing some useful methods for working with Haxe compiled Phantom
+  code, especially code evaluated inside of pages.
+ **/
 class PhantomTools {
 
     /**
       Injects the currently executing phantomjs script into a webkit page.
       Returns the return value from page.injectJs.
      **/
-    public static function injectThis(page:WebPage){
+    public static function injectThis(page:WebPage):Bool{
         return page.injectJs(System.args[0]);
     }
 
     /**
-      Simple routine which detects if it is being evaluated in webkit.
-      Note: The page must first be injected with injectThis.
+      Sets a window.onerror handler that will always return false (if
+      appropriate).  This will ensure that page errors are captured by Phantom
+      error handlers.
      **/
-    inline public static function inPhantom(){
-        return untyped window.hasOwnProperty('phantom');
+    public static function exposeWindowErrors(page:WebPage):Bool{
+        return page.evaluate(function(){
+	    if( untyped __js__("typeof window") != "undefined" ) {
+                untyped window.onerror = function() return false;
+	    }
+	    return true;
+        });
     }
+
+    /**
+      Simple routine which detects if it is running inside of the main Phantom
+      scope.
+     **/
+    inline public static function inPhantom():Bool{
+        if( untyped __js__("typeof window") != "undefined" ) {
+            return untyped window.hasOwnProperty('phantom');
+        } else return false;
+    }
+
 }
