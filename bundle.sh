c133ebc837501b90a42188a6969c71f97fa19225
Justin Donaldson <jdonaldson@gmail.com>
2013-05-15 22:42:38 -0700

    get rid of some vim temp files in bundle.sh

--- bundle.sh
+++ bundle.sh
@@ -3,4 +3,4 @@ rm phantomjs.zip
 cp -r src/js haxelib
 
 haxe -cp src -js x --no-output -xml haxelib/haxedoc.xml `cd src && ls js/phantomjs/*.hx`
-zip -r phantomjs.zip haxelib -x "*.DS_Store"
+zip -r phantomjs.zip haxelib -x "*.DS_Store" "*.un~"
